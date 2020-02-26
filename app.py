69265a48070c9052aade68f05a55c60d294554b3
Patrick Tobler <pt017@hdm-stuttgart.de>
2020-02-27 00:56:54 +0800

    Added Build Order page.

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def display_index():
 def display_imprint():
     return render_template('imprint.html')
 
+@app.route('/buildorder')
+def display_buildorder():
+    return render_template('buildorder.html')
+
 
 if __name__ == '__main__':
     app.run()
